73a95e5f6fc71a29576530b976d99522f0553ff0
Felipe Castellanos S <felipecs5303@hotmail.com>
2022-03-26 19:04:42 -0500

    DB context y gitignore agregados

---
+++ MaquinaDispensadora/MaquinaDispensadoraContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BD
+{
+    public partial class MaquinaDispensadoraContext : DbContext
+    {
+        public MaquinaDispensadoraContext()
+        {
+        }
+
+        public MaquinaDispensadoraContext(DbContextOptions<MaquinaDispensadoraContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Marca> Marcas { get; set; } = null!;
+        public virtual DbSet<Producto> Productos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=FELIPE-DESKTOP\\SQLEXPRESS; Database=MaquinaDispensadora; Trusted_Connection= True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Marca>(entity =>
+            {
+                entity.HasKey(e => e.IdMarca);
+
+                entity.Property(e => e.Nombre).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.HasKey(e => e.IdProducto);
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.MarcaNavigation)
+                    .WithMany(p => p.Productos)
+                    .HasForeignKey(d => d.Marca)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Productos_Marcas");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ MaquinaDispensadora/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BD
+{
+    public partial class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; } = null!;
+        public int Marca { get; set; }
+        public int Precio { get; set; }
+        public int UnidadesDisp { get; set; }
+
+        public virtual Marca MarcaNavigation { get; set; } = null!;
+    }
+}
